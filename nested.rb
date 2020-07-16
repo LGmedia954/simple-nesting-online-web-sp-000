eddb8967d1a7e1a738f6ec160a1e305bcec5c2ca
Leslie G <lesliegonzalez@me.com>
2020-07-16 03:24:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -91,25 +91,6 @@ def adding_matz
 end
 
 
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
-    
-end
-
 def changing_alan
 	programmer_hash = 
  		{
